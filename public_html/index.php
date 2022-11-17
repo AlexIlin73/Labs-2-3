670862b75df7839720e9851fd167c3bcba39e964
AlexIlin73 <kleyd2002@bk.ru>
2022-11-17 12:38:15 +0400

    лабы по инет програмированию

---
+++ public_html/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Мои лабы </title>
+ 
+</head>
+
+<body>
+  <main>
+    <div class="container d-flex flex-column align-items-center pt-5">
+      <h1 class="block"><span class="badge badge-primary"> ИСТбд-31 Ильин Александр </span></h1>
+      <h2 class="block"><a href="L2.php" class="badge badge-success">Лабораторная №2</a></h2>
+      <h2 class="block"><a href="L3.php" class="badge badge-success">Лабораторная №3</a></h2>
+      
+     <div>
+  </main>
+</body>
+
+</html>
